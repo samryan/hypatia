7a7591f9211e54cb9a81b69edf3961b10b8bd2e5
Sam Ryan <ryansam@amazon.com>
2016-08-22 22:31:31 -0700

    tweak php templates

--- single.php
+++ single.php
@@ -17,7 +17,7 @@ get_header(); ?>
 
 			get_template_part( 'template-parts/content', get_post_format() );
 
-//			the_post_navigation();
+			the_post_navigation();
 
 		endwhile; // End of the loop.
 		?>
