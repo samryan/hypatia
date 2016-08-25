089f39fc4996dd00d1baecdf521b943088f692b4
Sam Ryan <ryansam@amazon.com>
2016-08-24 17:15:53 -0700

    remove a ton of wp default tags from the <head> tag

--- functions.php
+++ functions.php
@@ -134,11 +134,43 @@ require get_template_directory() . '/inc/extras.php';
  */
 require get_template_directory() . '/inc/customizer.php';
 
+
+
 /**
- * Stop printing the default wp-emoji stuff in the <head> tag.
+ * Stop printing non-useful wordpress stuff in the <head> tag.
  */
 
-remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
-remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
-remove_action( 'wp_print_styles', 'print_emoji_styles' );
-remove_action( 'admin_print_styles', 'print_emoji_styles' );
+function remove_wp_junk() {
+  remove_action('wp_head', 'rsd_link');
+  remove_action('wp_head', 'wp_generator');
+  remove_action('wp_head', 'feed_links', 2);
+  remove_action('wp_head', 'index_rel_link');
+  remove_action('wp_head', 'wlwmanifest_link');
+  remove_action('wp_head', 'feed_links_extra', 3);
+  remove_action('wp_head', 'start_post_rel_link', 10, 0);
+  remove_action('wp_head', 'parent_post_rel_link', 10, 0);
+  remove_action('wp_head', 'adjacent_posts_rel_link', 10, 0);
+  remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0);
+  remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0);
+  remove_action('wp_head', 'print_emoji_detection_script', 7);
+  remove_action('set_comment_cookies', 'wp_set_comment_cookies');
+  remove_action('wp_head', 'wp_oembed_add_discovery_links');
+  remove_action('wp_head', 'wp_oembed_add_host_js');
+  remove_action('admin_print_scripts', 'print_emoji_detection_script');
+  remove_action('wp_print_styles', 'print_emoji_styles');
+  remove_action('admin_print_styles', 'print_emoji_styles');
+  remove_action('wp_head', 'rest_output_link_wp_head', 10);
+  remove_action('wp_head', 'wp_oembed_add_discovery_links', 10 );
+  remove_action('rest_api_init', 'wp_oembed_register_route');
+  remove_action('template_redirect', 'rest_output_link_header', 11, 0);
+  remove_filter('oembed_dataparse', 'wp_filter_oembed_result', 10);
+  add_filter('the_generator', '__return_false');
+  add_filter('emoji_svg_url', '__return_false');
+  add_filter('embed_oembed_discover', '__return_false');
+  add_filter('rewrite_rules_array', 'disable_embeds_rewrites');
+  add_filter('json_enabled', '__return_false');
+  add_filter('json_jsonp_enabled', '__return_false');
+  add_filter('rest_enabled', '__return_false');
+  add_filter('rest_jsonp_enabled', '__return_false');
+}
+add_action('after_setup_theme', 'remove_wp_junk');
