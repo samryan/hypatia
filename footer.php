62574b346714eaeaeebf9996e37e23621c5c7ec8
Sam Ryan <sam@samryan.net>
2017-02-04 22:08:43 -0800

    update footer

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
     <footer id="colophon" class="site-footer" role="contentinfo">
       <div class="site-info container">
-        <p>Content by Sam Ryan, 2007&ndash;<?php echo date('Y'); ?>. Most of the Amazon links use an <a href="https://affiliate-program.amazon.com">affiliate code</a>. Thanks for visiting.</p>
+        <p>Content by Sam Ryan, 2007&ndash;<?php echo date('Y'); ?>. Thanks for visiting!</p>
       </div>
     </footer>
   </div>
