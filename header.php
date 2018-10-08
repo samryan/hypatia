a598764d90911a80ce4a335454ed844c1720a279
Sam Ryan 👨🏻‍💻 <ryansam@amazon.com>
2018-10-08 14:06:14 -0700

    remove entypo, use inline svg for menu icon

--- header.php
+++ header.php
@@ -26,7 +26,9 @@
     <header class="site-header clear">
       <div class="container">
         <nav id="site-navigation" class="container main-navigation" role="navigation">
-          <span class="entypo-menu" id="toggle-menu"></span>
+          <span id="toggle-menu">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path fill="#FFF" d="M16.4 9H3.6c-.552 0-.6.447-.6 1 0 .553.048 1 .6 1h12.8c.552 0 .6-.447.6-1s-.048-1-.6-1zm0 4H3.6c-.552 0-.6.447-.6 1 0 .553.048 1 .6 1h12.8c.552 0 .6-.447.6-1s-.048-1-.6-1zM3.6 7h12.8c.552 0 .6-.447.6-1s-.048-1-.6-1H3.6c-.552 0-.6.447-.6 1s.048 1 .6 1z"/></svg>
+          </span>
           <a class="logo" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
           <ul>
             <li><a href="/blog">Blog</a></li>
