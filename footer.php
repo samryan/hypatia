363cc0430bf0e02a9b7ae9f492d486c23a09386e
Sam Ryan <sam@Sams-MacBook-Pro.local>
2018-10-14 08:48:09 -0700

    remove jquery, rewrite toggle menu in vanilla JS

--- footer.php
+++ footer.php
@@ -6,6 +6,8 @@
   </div>
 </div>
 <?php wp_footer(); ?>
+
+<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/hypatia.js"></script>
 <script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');ga('create', 'UA-233746-2', 'auto');ga('send', 'pageview');</script>
 <!--
 BENE LEGERE SAECLA VINCERE
--- js/hypatia.js
+++ js/hypatia.js
@@ -1,6 +1,8 @@
-$(document).ready(function(){
-  $('#toggle-menu').on('click', function(e) {
-    e.preventDefault();
-    $('nav ul').slideToggle(100);
-  });
-});
+var menu = document.querySelector('nav ul');
+var toggle = document.querySelector('#toggle-menu');
+
+toggle.addEventListener('click', function (event) {
+	event.preventDefault();
+  menu.classList.toggle('visible');
+  console.log(menu.classList);
+}, false);
