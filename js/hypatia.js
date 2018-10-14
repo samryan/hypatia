634df868efa6e4533b2b748862be97478ffddb80
Sam Ryan 👨🏻‍💻 <sam@samryan.net>
2018-10-14 08:59:06 -0700

    simplify toggle menu JS

--- js/hypatia.js
+++ js/hypatia.js
@@ -1,8 +1,4 @@
-var menu = document.querySelector('nav ul');
-var toggle = document.querySelector('#toggle-menu');
-
-toggle.addEventListener('click', function (event) {
-	event.preventDefault();
-  menu.classList.toggle('visible');
-  console.log(menu.classList);
+document.querySelector('#toggle-menu').addEventListener('click', function (e) {
+	e.preventDefault();
+  document.querySelector('nav ul').classList.toggle('visible');
 }, false);
